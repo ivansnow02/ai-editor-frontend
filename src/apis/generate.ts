b4e246994d17c4bfc6835b959bd0439c16a3886f
Ivan Snow <ivansnow02@gmail.com>
2024-06-21 14:27:57 +0800

    refactor: 将VuetifyTiptap移到CustomEditor.vue

--- src/apis/generate.ts
+++ src/apis/generate.ts
@@ -1,4 +1,4 @@
-import { request, aiRequest } from '@/utils/request'
+import { aiRequest, request } from '@/utils/request'
 import { fetchEventSource } from '@microsoft/fetch-event-source'
 
 export const getCompletion = (formData: any) =>
@@ -17,12 +17,12 @@ export const getFileSummary = (formData: any) =>
 
 /**
  * 发送一个 POST 请求到指定的端点，并建立一个服务器发送事件连接以接收流数据。
- * 
+ *
  * @param formData - 要发送到请求体中的数据。
  * @param func - 要附加到端点 URL 的功能名。
  * @param onMessage - 用于处理接收到的数据的回调函数。
  */
-export const getStream = async (formData: Any, func: string, onMessage: Function) => {
+export const getStream = async (formData: any, func: string, onMessage: Function) => {
   await fetchEventSource(`http://127.0.0.1:8000/api/langserve/${func}/stream`, {
     method: 'POST',
     headers: {
@@ -31,8 +31,12 @@ export const getStream = async (formData: Any, func: string, onMessage: Function
     body: JSON.stringify(formData),
     onmessage: (event) => {
       if (event.event === 'data') {
-        onMessage(JSON.parse(event.data).content);
+        onMessage(JSON.parse(event.data).content)
       }
+    },
+    onerror: (event) => {
+      // 关闭连接
+      event.target.close()
     }
   })
 }
